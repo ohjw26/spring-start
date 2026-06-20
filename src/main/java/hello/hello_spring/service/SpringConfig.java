05b7bbcb46dac878de979fa92eeb10c6a11d019a
Jiwon Oh <ohjiwon3040@gmail.com>
2026-06-21 00:01:41 +0900

    스프링 DB 접근 기술(순수 JDBC)

--- src/main/java/hello/hello_spring/service/SpringConfig.java
+++ src/main/java/hello/hello_spring/service/SpringConfig.java
@@ -1,12 +1,20 @@
 package hello.hello_spring.service;
 
+import hello.hello_spring.repository.JdbcMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +23,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+ //       return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
