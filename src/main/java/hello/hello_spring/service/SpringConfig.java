dbe0cb54c2b279d48f4a677ede784e9e4e6b5772
Jiwon Oh <ohjiwon3040@gmail.com>
2026-08-02 23:23:25 +0900

    AOP

--- src/main/java/hello/hello_spring/service/SpringConfig.java
+++ src/main/java/hello/hello_spring/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hello_spring.service;
 
+import hello.hello_spring.aop.TimeTraceAop;
 import hello.hello_spring.repository.*;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,11 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 
 //    @Bean
 //    public MemberRepository memberRepository() {
